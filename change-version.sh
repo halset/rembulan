080497a36e4c1e31ac1b9a99f43646fb7bf4f7c0
Tore Halset <halset@pvv.ntnu.no>
2021-08-17 13:15:23 +0200

    change groupId on maven build as well

--- change-version.sh
+++ change-version.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-OLD=0.3.3-SNAPSHOT
-NEW=0.3.3
+OLD=0.3.3
+NEW=0.3.4-SNAPSHOT
 
 for f in `find . -not -path "./.git/*" -not -path "./.gradle/*" \( ! -name change-version.sh \) -type f -exec grep -l $OLD {} \;`
 do
