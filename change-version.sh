1917efed2d2d66046634893d391a0de609341a53
Tore Halset <halset@pvv.ntnu.no>
2023-08-15 16:15:46 +0200

    0.3.5

--- change-version.sh
+++ change-version.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-OLD=0.3.4
-NEW=0.3.5-SNAPSHOT
+OLD=0.3.5-SNAPSHOT
+NEW=0.3.5
 
 for f in `find . -not -path "./.git/*" -not -path "./.gradle/*" \( ! -name change-version.sh \) -type f -exec grep -l $OLD {} \;`
 do
